b69b23e595ed77b9646d522f8598de96f72d87c0
Fernando <fernando.santos@datatype.com.br>
2022-11-20 14:08:45 -0300

    first commit

---
+++ src/data-source.ts
@@ -0,0 +1,14 @@
+import "dotenv/config"
+import "reflect-metadata"
+import { DataSource } from "typeorm";
+const port = process.env.DB_PORT as number | undefined
+
+export const ApppDataSource = new DataSource({
+    type:"postgres",
+    host: process.env.DB_HOST,
+    port: port,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+
+})
