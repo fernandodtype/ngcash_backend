f369c2d8eb298e36952593b21b069baeeea28f31
Fernando <fernando.santos@datatype.com.br>
2022-12-13 09:49:13 -0300

    feat: swagger api da documentação

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import { ApppDataSource } from "./data-source"
 import routes from "./routes";
+const swaggerUi =  require("swagger-ui-express")
+const swaggerDocs = require("./swagger.json")
 
 ApppDataSource.initialize().then(() => {
     const app = express();
 
     app.use(express.json())
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
     
     app.use(routes)
 
